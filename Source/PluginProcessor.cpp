4073859b59bb2f5e54b55a67e049917e491df9a9
Moira Leon <moira.leon2@gmail.com>
2021-11-10 17:14:44 -0700

    Low Cut Slope + High Cut Slope

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -213,6 +213,17 @@ juce::AudioProcessorValueTreeState::ParameterLayout  SimpleEQAudioProcessor::cre
                                                            juce::NormalisableRange<float>(0.1f,10.f,0.05f,1.f),
                                                            1.f ));
 
+    juce::StringArray stringArray;
+    for (int i =0; i<4; ++i) {
+        juce::String str;
+        str << (12 + i*12);
+        str << "db/Oct";
+        stringArray.add(str);
+    }
+    
+    layout.add(std::make_unique<juce::AudioParameterChoice>("LowCut Slope","LowCut Slope", stringArray,0));
+    layout.add(std::make_unique<juce::AudioParameterChoice>("HighCut Slope","HighCut Slope", stringArray,0));
+
     
     return layout;
     
