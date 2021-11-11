999d75d0d046990a13e7b88c3af0810148ec98d4
Moira Leon <moira.leon2@gmail.com>
2021-11-10 17:02:00 -0700

    peak frequency

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -188,9 +188,20 @@ juce::AudioProcessorValueTreeState::ParameterLayout  SimpleEQAudioProcessor::cre
 
     juce::AudioProcessorValueTreeState::ParameterLayout layout;
     
-    layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq","LowCut Freq", juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),20.f ));
+    layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq",
+                                                           "LowCut Freq",
+                                                           juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),
+                                                           20.f ));
     
-    layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq","HighCut Freq", juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),20000.f ));
+    layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq",
+                                                           "HighCut Freq",
+                                                           juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),
+                                                           20000.f ));
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Freq",
+                                                           "Peak Freq",
+                                                           juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),
+                                                           750.f ));
     
     return layout;
     
